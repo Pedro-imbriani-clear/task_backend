a916b1c7fab29f861fd995b2e7f7cee72789f406
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-08-29 00:25:49 -0300

    Middleware & DB with Knex

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
-AppRegistry.registerComponent(appName, () => App);
+app.db = db
+
+app.listen(3000, () => {
+    console.log('Backend executando...')
+})
